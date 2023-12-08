a2f896982e1f51ad1805fe60dab1d97fab7c2622
Marcelo Biotto <lee.am@hotmail.com>
2023-12-07 21:40:23 -0300

    adicionando o card para adicionar os cards

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 
   // const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${numero}`);
-  async function pokeAll(){
-  const apiUrl = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
- const apiConvertida =  await apiUrl.json();
- console.log(apiConvertida);
-}
-pokeAll();
+  
+
+  
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+    .then(response => response.json())
+    .then((data) => {
+        // Imprime os dados brutos recebidos da PokeAPI
+        console.log(data);
+
+        // Itera sobre a lista de resultados e imprime os nomes dos pokémons
+        data.results.forEach((pokemon) => {
+            console.log(pokemon.name);
+        });
+    })
+    .catch(error => console.error(error));
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //função para detalhes do pokemon especifico
 
   let pokeImagem = document.querySelector('#pkmn');
   let pkmName = document.querySelector('#pokeName');
