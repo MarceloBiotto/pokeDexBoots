600fbfa7377412638aa3fcb99ebc4b82f1ac1cd3
Marcelo Biotto <lee.am@hotmail.com>
2023-12-11 13:13:36 -0300

    comitando

--- js/script.js
+++ js/script.js
@@ -8,46 +8,43 @@
   let  pkWeight = document.querySelector('#weight')
 
   
-
   async function buscarEMostrarPokemons() {
-    try{
-    let capturaName = document.querySelector('.descricao-pokemon');
-    let containerPokemon = document.querySelector('.container-pokemons');
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
-    const data = await response.json();
+    try {
+      let containerPokemon = document.querySelector('.container-pokemons');
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
+      const data = await response.json();
   
-    data.results.forEach(async (pokemon) => {
-      const pokemonResponse = await fetch(pokemon.url);
-      const pokemonData = await pokemonResponse.json();
-    
-      containerPokemon.innerHTML += `
-       
-        <li class=" pokemon__lista card align-items-center text-center d-flex col-4 m-2">
+      data.results.forEach(async (pokemon) => {
+        const pokemonResponse = await fetch(pokemon.url);
+        const pokemonData = await pokemonResponse.json();
+  
+        const pokemonElement = document.createElement('li');
+        pokemonElement.classList.add('pokemon__lista', 'card', 'align-items-center', 'text-center', 'd-flex', 'col-4', 'm-2');
+        pokemonElement.innerHTML = `
           <div class="descricao-pokemon">
-          
             <p>ID: ${pokemonData.id}</p>
-            <p>Name: <span class= "pokemon-name">${pokemonData.name}</span></p>
+            <p>Name: <span class="pokemon-name">${pokemonData.name}</span></p>
             <img class="img-pokemon img-fluid" src="${pokemonData.sprites.front_default}" alt="imagem pokemon">
-            <p>Type: ${pokemonData.types[0].type.name}
+            <p>Type: ${pokemonData.types[0].type.name}</p>
           </div>
-        </li>
-       
-      `;
-
-      
-      containerPokemon.addEventListener('click', () => {
-        const pokemonClicado = containerPokemon.querySelector('.pokemon-name').textContent; // falta acertar aqui, capturar o input do name
-        // para na proxima pagina pegar esse valor e concatenar com o 'https://pokeapi.co/api/v2/pokemon/' => aqui vai o valor concatenado
-        localStorage.setItem('namePokemon', pokemonClicado);
-      
-    });
-  });
-  }catch(error){
-      containerPokemon.innerHTML += `<p> Houve um erro ao carregar os pokemons: ${error}</p>`
+        `;
+  
+        containerPokemon.appendChild(pokemonElement);
+  
+        // Adicionar o event listener para cada elemento Pokémon
+        pokemonElement.addEventListener('click', () => {
+          const pokemonClicado = pokemonData.name;
+          localStorage.setItem('namePokemon', pokemonClicado);
+        });
+      });
+  
+    } catch (error) {
+      containerPokemon.innerHTML += `<p>Houve um erro ao carregar os pokemons: ${error}</p>`;
+    }
   }
-}
   
   buscarEMostrarPokemons();
+  
   
   
   
