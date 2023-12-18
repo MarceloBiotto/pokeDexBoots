4daaf7e9d19c39a7990b9d8a74cdd0a9d47a0bfd
Marcelo Biotto <lee.am@hotmail.com>
2023-12-18 09:40:48 -0300

    atualizando a pokeUptade

--- js/detalhes.js
+++ js/detalhes.js
@@ -1,100 +1,118 @@
 let pkType1 = document.querySelector('#pokeType1');
 let baseHp = document.querySelector('.statHp');
 let baseAtk = document.querySelector('.statAtk');
-let baseDef = document.querySelector('.statDef')
-let baseSpatk = document.querySelector('.statSpatk')
-let baseSpDef = document.querySelector('.statSdef')
+let baseDef = document.querySelector('.statDef');
+let baseSpatk = document.querySelector('.statSpatk');
+let baseSpDef = document.querySelector('.statSdef');
 let baseSpeed = document.querySelector('.statSpeed');
 let buttonStat = document.querySelector('.button__stats');
 let mudaCard = document.querySelectorAll('.card-title');
 let weaknessesText = document.querySelector('.textfraqueza');
-let buttonFraq = document.querySelector('.button__fraqueza')
-let fraquezas = document.querySelector('.fraqueza');
+let buttonFraq = document.querySelector('.button__fraqueza');
 let fraquezasList = document.querySelector('.fraquezas-list');
-let uniqueWeaknesses;
 
-async function pokeInfo(){
-    const pokemonGerado =   localStorage.getItem('idPokemon');
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonGerado}`);  
-    const poke = await response.json();
-   
+async function updateCard(pokeData) {
+  pkmName.innerHTML = pokeData.name;
+  pokeImagem.src =
+    pokeData['sprites']['versions']['generation-v']['black-white']['animated'][
+      'front_default'
+    ] || pokeData.sprites.front_default;
+  if (pokeImagem.src == pokeData.sprites.front_default) {
+    pkmImagem.style.width = '150px';
+  }
+  pkId.innerHTML = pokeData.id;
+  pkWeight.innerHTML = pokeData.weight;
 
+  let listaStatsAtributes = pokeData.stats.map((statsPoke) => statsPoke.base_stat);
 
+  let hpConvertidoUptade = listaStatsAtributes[0];
+  let atakConvertidoUptade = listaStatsAtributes[1];
+  let defConvertidoUptade = listaStatsAtributes[2];
+  let spatkConvertidoUptade = listaStatsAtributes[3];
+  let spdefConvertidoUptade = listaStatsAtributes[4];
+  let speedConvertidoUptade = listaStatsAtributes[5];
 
+  baseHp.innerHTML = hpConvertidoUptade;
+  baseAtk.innerHTML = atakConvertidoUptade;
+  baseDef.innerHTML = defConvertidoUptade;
+  baseSpatk.innerHTML = spatkConvertidoUptade;
+  baseSpDef.innerHTML = spdefConvertidoUptade;
+  baseSpeed.innerHTML = speedConvertidoUptade;
 
-    pkmName.innerHTML += poke.name;
-    pokeImagem.src = poke['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] || poke.sprites.front_default;
-    if(pokeImagem.src == poke.sprites.front_default){ // verificar essa logica
-      pkmImagem.style.width =  "50%";
-    }
+  pkType1.innerHTML = pokeData.types.map((typePoke) => typePoke.type.name).join(', ');
 
-    pkId.innerHTML += poke.id;
-    pkWeight.innerHTML += poke.weight;
-    pkType1.innerHTML += poke.types.map(typePoke => typePoke.type.name);
-    // hp.innerHTML += poke.stats.map(statsPoke => statsPoke.base_stat)[0];
-    // atk.innerHTML += poke.stats.map(statsPoke => statsPoke.base_stat)[1];
-    let listaStats =poke.stats.map(statsPoke => statsPoke.base_stat);
-    console.log("eu sou a universal: " ,listaStats[3])
-    let hpConvertido = listaStats[0];
-    let atkConvertido = listaStats[1];
-    let defConvertido = listaStats[2];
-    let spAtkConvertido = listaStats[3];
-    let spDefConvertido = listaStats[4];
-    let speedConvertido = listaStats[5];
+  localStorage.setItem('idPokemon', pkId);
 
-  
+  await showWeaknesses(pokeData);
+}
 
-    baseHp.innerHTML += hpConvertido;
-    baseAtk.innerHTML += atkConvertido;
-    baseDef.innerHTML += defConvertido
-    baseSpatk.innerHTML += spAtkConvertido;
-    baseSpDef.innerHTML += spDefConvertido;
-    baseSpeed.innerHTML += speedConvertido;
+async function pokeInfo() {
+  const pokemonGerado = localStorage.getItem('idPokemon');
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonGerado}`);
+  const poke = await response.json();
 
-    
+  pkmName.innerHTML += poke.name;
+  pokeImagem.src =
+    poke['sprites']['versions']['generation-v']['black-white']['animated'][
+      'front_default'
+    ] || poke.sprites.front_default;
+  if (pokeImagem.src == poke.sprites.front_default) {
+    pkmImagem.style.width = '50%';
+  }
 
-    buttonStat.addEventListener('click', (e) => {
-      e.preventDefault();
-      mudaCard.forEach(card => {
-        card.classList.toggle('hidden');
-      });
+  pkId.innerHTML += poke.id;
+  pkWeight.innerHTML += poke.weight;
+  pkType1.innerHTML += poke.types.map((typePoke) => typePoke.type.name);
+
+  let listaStats = poke.stats.map((statsPoke) => statsPoke.base_stat);
+  let hpConvertido = listaStats[0];
+  let atkConvertido = listaStats[1];
+  let defConvertido = listaStats[2];
+  let spAtkConvertido = listaStats[3];
+  let spDefConvertido = listaStats[4];
+  let speedConvertido = listaStats[5];
+
+  baseHp.innerHTML += hpConvertido;
+  baseAtk.innerHTML += atkConvertido;
+  baseDef.innerHTML += defConvertido;
+  baseSpatk.innerHTML += spAtkConvertido;
+  baseSpDef.innerHTML += spDefConvertido;
+  baseSpeed.innerHTML += speedConvertido;
+
+  buttonStat.addEventListener('click', (e) => {
+    e.preventDefault();
+    mudaCard.forEach((card) => {
+      card.classList.toggle('hidden');
+    });
+  });
+
+  buttonFraq.addEventListener('click', async (e) => {
+    e.preventDefault();
+    mudaCard.forEach((card) => {
+      card.classList.toggle('hidden2');
     });
 
-    buttonFraq.addEventListener('click', async (e) => {
-      e.preventDefault();
-      mudaCard.forEach(card => {
-        card.classList.toggle('hidden2');
-      });
-    
-      await showWeaknesses();
-    });
-    
-    async function showWeaknesses() {
-      await fraquezas();
-      fraquezasList.innerHTML = "";
-    
-      if (localStorage.getItem('fraqueza')) {
-        fraquezasList.innerHTML = localStorage.getItem('fraqueza');
-      }
-    }
+    await showWeaknesses(poke);
+  });
+}
 
+async function showWeaknesses(pokeData) {
+  await fraquezas(pokeData);
+  fraquezasList.innerHTML = '';
 
+  if (localStorage.getItem('fraqueza')) {
+    fraquezasList.innerHTML = localStorage.getItem('fraqueza');
+  }
+}
 
-
-
-;
-
-async function fraquezas() {
+async function fraquezas(pokeData) {
   try {
-    const weaknessesPromises = poke.types.map(async (type) => {
+    const weaknessesPromises = pokeData.types.map(async (type) => {
       const typeUrl = type.type.url;
       const response = await fetch(typeUrl);
       const typeDetails = await response.json();
-      return typeDetails.damage_relations.double_damage_from.map(
-        (weakness) => weakness.name
-      );
+      return typeDetails.damage_relations.double_damage_from.map((weakness) => weakness.name);
     });
-
 
     const allWeaknesses = await Promise.all(weaknessesPromises);
 
@@ -103,119 +121,36 @@ async function fraquezas() {
     const uniqueWeaknesses = [...new Set(allWeaknesses.flat())];
 
     const weaknessesText =
-      uniqueWeaknesses.length > 0
-        ? `<p>Fraquezas:${uniqueWeaknesses.join(", ")}</p>`
-        : "";
+      uniqueWeaknesses.length > 0 ? `<p>Fraquezas:${uniqueWeaknesses.join(', ')}</p>` : '';
 
     localStorage.setItem('fraqueza', weaknessesText);
   } catch (error) {
-    console.error("Erro ao obter fraquezas:", error);
+    console.error('Erro ao obter fraquezas:', error);
   }
 }
 
-await fraquezas();
+async function pokeUpdate() {
+  const responseUptade = await fetch(`https://pokeapi.co/api/v2/pokemon/${currentId}`);
+  const pokeUptadeAtribute = await responseUptade.json();
+  await updateCard(pokeUptadeAtribute);
 }
 
+let idPokemonGerado = localStorage.getItem('idPokemon');
+document.addEventListener('DOMContentLoaded', () => {
+  const previousButton = document.getElementById('previous-btn');
+  const nextButton = document.getElementById('next-btn');
 
-
-    
-   
-
-  //  pokeInfo();
-
-
-
-   async function pokeUpdate() {
-    const responseUptade = await fetch(`https://pokeapi.co/api/v2/pokemon/${currentId}`);
-    const pokeUptadeAtribute = await responseUptade.json();
-
-
-    pkmName.innerHTML = pokeUptadeAtribute.name;
-    pokeImagem.src = pokeUptadeAtribute['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] ||  pokeUptadeAtribute.sprites.front_default;
-    let pkmImagem= document.querySelector('#pkmn');
-    if(pokeImagem.src == pokeUptadeAtribute.sprites.front_default){
-      pkmImagem.style.width =  "150px"; // verificar essa logica
-    }
-    pkId.innerHTML = pokeUptadeAtribute.id;
-    pkWeight.innerHTML = pokeUptadeAtribute.weight;
-
-    
-
-    let listaStatsAtributes =pokeUptadeAtribute.stats.map(statsPoke => statsPoke.base_stat); //aqui esta a lista para não perde-la
-
-    let  hpConvertidoUptade = listaStatsAtributes[0];
-    let  atakConvertidoUptade = listaStatsAtributes[1];
-    let  defConvertidoUptade = listaStatsAtributes[2];
-    let  spatkConvertidoUptade = listaStatsAtributes[3];
-    let  spdefConvertidoUptade = listaStatsAtributes[4];
-    let  speedConvertidoUptade = listaStatsAtributes[5];
-
-    baseHp.innerHTML = hpConvertidoUptade;
-    baseAtk.innerHTML = atakConvertidoUptade;
-    baseDef.innerHTML = defConvertidoUptade;
-    baseSpatk.innerHTML = spatkConvertidoUptade;
-    baseSpDef.innerHTML = spdefConvertidoUptade;
-    baseSpeed.innerHTML =  speedConvertidoUptade;
-
-    pkType1.innerHTML = pokeUptadeAtribute.types.map(typePoke => typePoke.type.name).join(', ');
-  
-
-    localStorage.setItem('idPokemon', pkId);
-
-    async function fraquezas() {
-      try {
-        const weaknessesPromises = pokeUptadeAtribute.types.map(async (type) => {
-          const typeUrl = type.type.url;
-          const response = await fetch(typeUrl);
-          const typeDetails = await response.json();
-          return typeDetails.damage_relations.double_damage_from.map(
-            (weakness) => weakness.name
-          );
-        });
-    
-    
-        const allWeaknesses = await Promise.all(weaknessesPromises);
-    
-        console.log(allWeaknesses);
-    
-        const uniqueWeaknesses = [...new Set(allWeaknesses.flat())];
-    
-        const weaknessesText =
-          uniqueWeaknesses.length > 0
-            ? `<p>Fraquezas:${uniqueWeaknesses.join(", ")}</p>`
-            : "";
-    
-        localStorage.setItem('fraqueza', weaknessesText);
-      } catch (error) {
-        console.error("Erro ao obter fraquezas:", error);
-      }
-    }
-    
-    await fraquezas();
-    }
-
-
-let idPokemonGerado =   localStorage.getItem('idPokemon');
-document.addEventListener("DOMContentLoaded", () => {
-  const previousButton = document.getElementById("previous-btn");
-  const nextButton = document.getElementById("next-btn");
-
-  previousButton.addEventListener("click", async () => {
+  previousButton.addEventListener('click', async () => {
     if (currentId > 1) {
       currentId--;
-      
       await pokeUpdate();
     }
   });
 
-  nextButton.addEventListener("click", async () => {
+  nextButton.addEventListener('click', async () => {
     currentId++;
-    
     await pokeUpdate();
   });
 
-  
   currentId = idPokemonGerado;
- 
 });
-
